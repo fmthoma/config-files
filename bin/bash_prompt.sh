b01e50b3443e066fbc9735e95f492575a3c23b8d
Franz Thoma <franz.thoma@tngtech.com>
2015-04-17 20:26:37 +0200

    Pimp my Prompt

--- bin/bash_prompt.sh
+++ bin/bash_prompt.sh
@@ -18,10 +18,13 @@ function __ps1() {
     echo ${#PRINTING_CHARACTERS}
   }
 
-  function align() { # align left right
-    SPACES=$(($COLUMNS-$(length "$1")-$(length "$2")))
-    if [ $SPACES -lt 1 ]; then SPACES=1; fi
-    printf "%s%${SPACES}s%s" "$1" "" "$2"
+  function align() { # align left right fill_char=' '
+    CHAR=${3:-" "}
+    LEFT="$1"
+    RIGHT="$2"
+    SPACES=$(($COLUMNS-$(length "$LEFT")-$(length "$RIGHT")))
+    if [ $SPACES -lt 1 ]; then RIGHT='' SPACES=$(($COLUMNS-$(length "$LEFT"))); fi
+    echo "$LEFT$(printf "%${SPACES}s" "" | sed "s/\s/$CHAR/g")$RIGHT"
   }
 
   HISTORYITEM=$(history | tail -n1 | sed "s/\s*\([0-9]\+\).*/\1/")
@@ -38,29 +41,27 @@ function __ps1() {
   if [ $SHLVL -gt 1 ]; then LVL="+$(($SHLVL-1))"; fi
   TIME=$(date '+%H:%M')
   NEXTHISTORYITEM=$(($HISTORYITEM+1))
-  STATUSLINE_L="${STATUSLINE_L:+"$STATUSLINE_L "}$(item $BCyan  $OnBlack "$TIME $NEXTHISTORYITEM${LVL:+" $LVL"}")"
+  STATUSLINE_L="${STATUSLINE_L:+"$STATUSLINE_L━"}$(item $BCyan  "" "$TIME $NEXTHISTORYITEM${LVL:+" $LVL"}")"
 
   # User@Host
-  #STATUSLINE_L="${STATUSLINE_L:+"$STATUSLINE_L "}$(item $BGreen $OnBlack "\u@\h")"
+  #STATUSLINE_L="${STATUSLINE_L:+"$STATUSLINE_L━"}$(item $BGreen "" "\u@\h")"
 
   # PWD
-  STATUSLINE_L="${STATUSLINE_L:+"$STATUSLINE_L "}$(item $BBlue  $OnBlack "$(pwd)")"
+  STATUSLINE_L="${STATUSLINE_L:+"$STATUSLINE_L━"}$(item $BBlue  "" "$(pwd)")"
 
   # Git
-  STATUSLINE_L="${STATUSLINE_L:+"$STATUSLINE_L "}$(item $BRed   $OnBlack "$(__git_ps1 '%s')")"
+  STATUSLINE_L="${STATUSLINE_L:+"$STATUSLINE_L━"}$(item $BRed   "" "$(__git_ps1 '%s')")"
 
   # Stats
   MEMFREE=$( bc -l <<< "scale=1;`sed -n "s/MemFree:[\t ]\+\([0-9]\+\) kB/\1/p" /proc/meminfo`/1024/1024" )
   MEMTOTAL=$( bc -l <<< "scale=1;`sed -n "s/MemTotal:[\t ]\+\([0-9]\+\) kB/\1/Ip" /proc/meminfo`/1024/1024" )
   LOAD=$(cat /proc/loadavg)
-  STATUSLINE_R="${STATUSLINE_R:+"$STATUSLINE_R "}$(item $Purple $OnBlack "$MEMFREE/${MEMTOTAL}GB | $LOAD")"
+  STATUSLINE_R="${STATUSLINE_R:+"$STATUSLINE_R "}$(item $Purple "" "$MEMFREE/${MEMTOTAL}GB | $LOAD")"
 
-  STATUSLINE="$(align "$STATUSLINE_L" "$STATUSLINE_R\[$Reset\]")"
-  echo "$STATUSLINE"
+  echo "$(align "$STATUSLINE_L" "$STATUSLINE_R\[$Reset\]" "━")"
 }
 
-PROMPT="\[$BWhite$OnBlack\]▶ \[$Reset\]"
+PROMPT="\[$BWhite\]▶ \[$Reset\]"
 
 PS1="$(__ps1)\n$PROMPT"
 PS2="$PROMPT"
-
