927e6bc607129cb51c8b88631afb2017b12245c8
Franz Thoma <franz.thoma@tngtech.com>
2015-08-25 00:39:29 +0200

    Housekeeping

--- lib/haskell-scripts/Shell.hs
+++ lib/haskell-scripts/Shell.hs
@@ -3,16 +3,19 @@ module Shell (
     , module Shell.Dmenu
     , module Shell.History
 
-    , encode
-    , decode
+    , encode , decode
+    , foldIO , fold
+    , uniq
 ) where
 
+import qualified Control.Foldl as Fold
 import Data.Text (pack, unpack)
 import Filesystem.Path.CurrentOS (encodeString, decodeString)
-import Prelude hiding (FilePath)
+import Prelude hiding (FilePath, nub)
 import Shell.Dmenu
 import Shell.History
-import Turtle
+import Turtle hiding (foldIO, fold)
+import qualified Turtle as StupidTurtle
 
 
 
@@ -21,3 +24,14 @@ encode = pack . encodeString
 
 decode :: Text -> FilePath
 decode = decodeString . unpack
+
+
+foldIO :: MonadIO io => FoldM IO a r -> Shell a -> io r
+foldIO = flip StupidTurtle.foldIO
+
+fold :: MonadIO io => Fold a b -> Shell a -> io b
+fold = flip StupidTurtle.fold
+
+
+uniq :: Ord a => Shell a -> Shell a
+uniq = fold Fold.nub >=> select
