800ad3639203fe41afba206158c42a1030218eb7
Franz Thoma <franz.thoma@tngtech.com>
2015-11-13 22:52:30 +0100

    Extract completion.zsh

---
+++ zsh/completion.zsh
@@ -0,0 +1,22 @@
+# The following lines were added by compinstall
+
+zstyle ':completion:*' completer _expand _complete _ignored
+zstyle ':completion:*' completions 1
+zstyle ':completion:*' expand prefix suffix
+zstyle ':completion:*' glob 1
+zstyle ':completion:*' ignore-parents parent pwd .. directory
+zstyle ':completion:*' insert-unambiguous true
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-suffixes true
+zstyle ':completion:*' menu select
+zstyle ':completion:*' original true
+zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
+zstyle ':completion:*' substitute 1
+zstyle :compinstall filename "$DOTFILES/zsh/completion.zsh"
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
+
+setopt extendedglob
+setopt nomatch
