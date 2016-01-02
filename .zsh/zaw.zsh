6a000071d9453df292a2cf6c644d7244985d73e7
Franz Thoma <franz.thoma@tngtech.com>
2016-01-02 13:57:35 +0100

    Use zaw for history

--- .zsh/zaw.zsh
+++ .zsh/zaw.zsh
@@ -1,6 +1,10 @@
 source "$(dirname $0)/zaw/zaw.zsh"
 
-zstyle ':filter-select:highlight' matched fg=green
-zstyle ':filter-select' max-lines 8
+zstyle ':filter-select:highlight' matched fg=red,bold
+zstyle ':filter-select' max-lines 32
 zstyle ':filter-select' case-insensitive yes
 zstyle ':filter-select' extended-search yes
+
+bindkey "^R" zaw-history
+bindkey -M filterselect "^R" up-line-or-history
+bindkey -M filterselect "^M" accept-search
