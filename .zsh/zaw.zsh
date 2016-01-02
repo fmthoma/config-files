aa1de320adfd30d39db23982478dad73feb31dec
Franz Thoma <franz.thoma@tngtech.com>
2016-01-02 13:22:07 +0100

    Source zaw in .zshrc

---
+++ .zsh/zaw.zsh
@@ -0,0 +1,6 @@
+source "$(dirname $0)/zaw/zaw.zsh"
+
+zstyle ':filter-select:highlight' matched fg=green
+zstyle ':filter-select' max-lines 8
+zstyle ':filter-select' case-insensitive yes
+zstyle ':filter-select' extended-search yes
