3546779160e1f373e050a03504137cd1b71b462e
Franz Thoma <franz.thoma@tngtech.com>
2016-01-02 15:48:52 +0100

    Zaw history search with narrow-to-region

--- .zsh/zaw.zsh
+++ .zsh/zaw.zsh
@@ -5,6 +5,41 @@ zstyle ':filter-select' max-lines 32
 zstyle ':filter-select' case-insensitive yes
 zstyle ':filter-select' extended-search yes
 
-bindkey "^R" zaw-history
+function _zaw-history
+{
+    local state
+    MARK=CURSOR  # magick, else multiple ^R don't work
+
+    LB=$LBUFFER
+    LB=${LB##*&}
+    LB=${LB##*|}
+    LB=${LB##* }
+    LBUFFER=${LBUFFER%$LB}
+
+    RB=$RBUFFER
+    RB=${RB%%&*}
+    RB=${RB%%|*}
+    RB=${RB%% *}
+    RBUFFER=${RBUFFER#$RB}
+
+    PREFIX="$LBUFFER${BUFFER:+ }"
+    POSTFIX="${BUFFER:+ }$RBUFFER"
+
+    RBUFFER=" $RBUFFER"
+    #        ^ Add a space here (the first char is wrongly deleted in `narrow-to-region -R`)
+
+    narrow-to-region -p "$PREFIX" -P "$POSTFIX" -S state
+
+    BUFFER="$LB$RB"
+    LBUFFER="$LB"
+    RBUFFER="$RB"
+    zle zaw-history
+
+    narrow-to-region -R state
+}
+
+zle -N _zaw-history
+
+bindkey "^R" _zaw-history
 bindkey -M filterselect "^R" up-line-or-history
 bindkey -M filterselect "^M" accept-search
