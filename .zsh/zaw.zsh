34463f8394b8d398af81d799dc388ab134663360
Franz Thoma <franz.thoma@tngtech.com>
2016-01-03 19:27:56 +0100

    Ctrl-Space for path autocompletion of common commands

--- .zsh/zaw.zsh
+++ .zsh/zaw.zsh
@@ -7,6 +7,11 @@ zstyle ':filter-select' max-lines 32
 zstyle ':filter-select' case-insensitive yes
 zstyle ':filter-select' extended-search yes
 
+
+
+################################################################################
+# History                                                                      #
+################################################################################
 autoload -Uz narrow-to-region
 
 function _zaw-history
@@ -39,4 +44,54 @@ zle -N _zaw-history
 bindkey "^R" _zaw-history
 bindkey -M filterselect "^R" up-line-or-history
 
+
+
+################################################################################
+# Git branches                                                                 #
+################################################################################
 bindkey "^B" zaw-git-branches
+
+
+
+################################################################################
+# Autocompletion (fasd, git)                                                   #
+################################################################################
+function zaw-src-autocompletion () {
+    local lb rb buffer cmdln cmd args
+
+    actions=("zaw-callback-append-and-execute" "zaw-callback-append-to-buffer")
+    act_descriptions=("execute" "append to edit buffer")
+
+    lb="${LBUFFER##*(\||&)}"
+    lb="${lb#"${lb%%[\![:space:]]*}"}" # remove leading spaces
+    rb="${RBUFFER%%(\||&)*}"
+    rb="${rb%"${rb##*[\![:space:]]}"}" # remove trailing spaces
+    buffer="$lb$rb"
+    cmdln=( "${=buffer}" )
+
+    cmd="${cmdln[1]}"
+    args=( "${cmdln[2,-1]}" )
+
+    case "$cmd" in
+      "cd"|"j")
+        candidates=( $( fasd -dl ) )
+        ;;
+      "vi"|"less")
+        candidates=( $( fasd -fl ) )
+        ;;
+      *)
+        candidates=( $( fasd -al ) )
+    esac
+
+    BUFFER="$cmd"
+    options=( "-r" "-n" "-m" "-s" "$args" )
+}
+
+function zaw-callback-append-and-execute () {
+    BUFFER="$BUFFER ${(j:; :)@}"
+    zle accept-line
+}
+
+zaw-register-src -n autocompletion zaw-src-autocompletion
+
+bindkey "^@" zaw-autocompletion
