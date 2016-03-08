49e91808501c7dbb9457b8f38a1beeaefd9dd795
Franz Thoma <franz.thoma@tngtech.com>
2016-03-08 09:26:01 +0100

    Revert alias grep = vgrep

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -2,10 +2,7 @@ if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
 fi
 
-if which vgrep &> /dev/null;
-then alias grep='vgrep'
-else alias grep='grep --color=auto'
-fi
+alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 alias igrep='grep -i'
