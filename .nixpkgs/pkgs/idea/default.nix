1a10f2da2b528ca1a3ba6fdb1badb41edcaa5f09
Franz Thoma <franz.thoma@tngtech.com>
2017-07-05 12:45:05 +0200

    nixpkgs: Upgrade IntelliJ

--- .nixpkgs/pkgs/idea/default.nix
+++ .nixpkgs/pkgs/idea/default.nix
@@ -1,9 +1,9 @@
 {stdenv, pkgs, fetchurl}:
 pkgs.idea.idea-ultimate.overrideDerivation (super: rec {
   name = "idea-ultimate-${version}";
-  version = "2017.1.4";
+  version = "2017.1.5";
   src = fetchurl {
     url = "https://download.jetbrains.com/idea/ideaIU-${version}.tar.gz";
-    sha256 = "0a93fba480pvdh6x263fm7rb9w728smgx65nbvkkbdqngjmnanyx";
+    sha256 = "1z1df0hqcqmkkhi8h0y395xyzrm4q63yax9rcjhvnv7f7aj29353";
   };
 })
