92422a96064f38b8518592650fa5e0c0fc43fb81
Franz Thoma <franz.thoma@tngtech.com>
2017-10-29 14:00:51 +0100

    nixpkgs: dmenu: 4.6 -> 4.7

--- .nixpkgs/pkgs/dmenu/config.solarized-dark.h
+++ .nixpkgs/pkgs/dmenu/config.solarized-dark.h
@@ -4,15 +4,20 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka Expanded:size=12",
-	"monospace:size=12"
+	"monospace:size=10"
 };
-static const char *prompt      = "";       /* -p  option; prompt to the elft of input field */
-static const char *normbgcolor = "#002b36"; /* -nb option; normal background                 */
-static const char *normfgcolor = "#839496"; /* -nf option; normal foreground                 */
-static const char *selbgcolor  = "#073642"; /* -sb option; selected background               */
-static const char *selfgcolor  = "#93a1a1"; /* -sf option; selected foreground               */
-static const char *outbgcolor  = "#2aa198";
-static const char *outfgcolor  = "#002b36";
+static const char *prompt      = "";      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#839496", "#002b36" },
+	[SchemeSel] = { "#93a1a1", "#073642" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 40;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
