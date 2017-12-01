8b1896d6a94852fdadd0de892a0771bf39785832
Franz Thoma <franz.thoma@tngtech.com>
2017-12-01 15:00:58 +0100

    nixpkgs: Upgrade IntelliJ 2017.2.6 -> 2017.3

--- .nixpkgs/pkgs/idea/default.nix
+++ .nixpkgs/pkgs/idea/default.nix
@@ -1,9 +1,9 @@
 {stdenv, pkgs, fetchurl}:
 pkgs.idea.idea-ultimate.overrideDerivation (super: rec {
   name = "idea-ultimate-${version}";
-  version = "2017.2.6";
+  version = "2017.3";
   src = fetchurl {
     url = "https://download.jetbrains.com/idea/ideaIU-${version}.tar.gz";
-    sha256 = "1pjf6j3z8brqjx445ci70fiapdfzgbx3aiqc048lm12mp78l8psn";
+    sha256 = "09fk0x789llanm34xxsb8j4y5nmllvjs2rzqbcc5y4ad06sb4pgp";
   };
 })
