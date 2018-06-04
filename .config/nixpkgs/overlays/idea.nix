d19a4a1918778dd71863204f872d9262aaa8db1d
Franz Thoma <franz.thoma@tngtech.com>
2018-06-04 07:55:43 +0200

    nixpkgs: Upgrade IntelliJ 2018.1 -> 2018.1.4

--- .config/nixpkgs/overlays/idea.nix
+++ .config/nixpkgs/overlays/idea.nix
@@ -1,10 +1,10 @@
 self: super: {
   idea-ultimate = super.idea.idea-ultimate.overrideAttrs (oldAttrs: rec {
     name = "idea-ultimate-${version}";
-    version = "2018.1";
+    version = "2018.1.4";
     src = super.fetchurl {
       url = "https://download.jetbrains.com/idea/ideaIU-${version}.tar.gz";
-      sha256 = "0n98gjm3v7qdyd1hc82zg57gyhwbamf27dyal1z71xfav4z5zb10";
+      sha256 = "1122dhk7ad3a8iw7fs55kg7cjj657hcxbk2ykhd3x0h2759y25pv";
     };
   });
 }
