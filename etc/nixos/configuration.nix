ccfa365d3babe90c55844a0f9182683fc9ef7455
Franz Thoma <franz.thoma@tngtech.com>
2022-12-04 16:17:45 +0100

    New laptop

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -23,13 +23,14 @@
     plymouth.enable = true;
     plymouth.theme = "breeze";
     initrd.systemd.enable = true;
+    kernelParams = [ "nomodeset" ];
   };
 
   networking.hostName = "fthoma-nixos"; # Define your hostname.
   networking.extraHosts = ''
     # Fixes Java UnknownHostException
     127.0.0.1 fthoma-nixos
-  '' + import /etc/nixos/hosts.local.nix;
+  '' + (if builtins.pathExists /etc/nixos/hosts.local.nix then import /etc/nixos/hosts.local.nix else "");
   networking.firewall.allowedTCPPorts = [ 9100 ];
   networking.networkmanager.enable = true;
 
@@ -121,7 +122,7 @@
       lightdm.enable = true;
       defaultSession = "none+i3";
       autoLogin = {
-        user = "fthoma";
+        user = "thomaf";
         enable = true;
       };
       sessionCommands = ''
@@ -147,15 +148,15 @@
   services.prometheus.exporters.node.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.extraUsers.fthoma = {
+  users.extraUsers.thomaf = {
     isNormalUser = true;
-    group = "fthoma";
+    group = "thomaf";
     extraGroups = [ "networkmanager" "wheel" "docker" "adbusers" "dialout" ];
     uid = 1000;
     createHome = true;
     shell = pkgs.zsh;
   };
-  users.extraGroups.fthoma.gid = 1000;
+  users.extraGroups.thomaf.gid = 1000;
   users.defaultUserShell = pkgs.zsh;
   programs.adb.enable = true;
   programs.zsh = {
@@ -164,13 +165,18 @@
   };
   programs.gnupg.agent.enable = true;
 
-  # The NixOS release to be compatible with for stateful data such as databases.
-  system.stateVersion = "16.03";
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
 
-  virtualisation.docker.enable = true;
-  virtualisation.virtualbox.host = {
-    enable = true;
-    enableExtensionPack = true;
-    headless = false;
-  };
+  # virtualisation.docker.enable = true;
+  # virtualisation.virtualbox.host = {
+  #   enable = true;
+  #   enableExtensionPack = true;
+  #   headless = false;
+  # };
 }
