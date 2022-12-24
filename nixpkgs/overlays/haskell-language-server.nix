6412ff1fea17039e3459944d331eda8c4f82951c
Franz Thoma <franz.thoma@tngtech.com>
2022-12-24 05:21:44 +0100

    nipkgs: Clean up unused/obsolete overlays

--- nixpkgs/overlays/haskell-language-server.nix
+++
@@ -1,5 +0,0 @@
-self: super: {
-    haskell-language-server = super.haskell-language-server.override {
-        supportedGhcVersions =  [ "884" "8107" ];
-    };
-}
--- nixpkgs/overlays/nodejs.nix
+++
@@ -1,3 +0,0 @@
-self: super: {
-    yarn-12_x = super.yarn.override { nodejs = self.nodejs-12_x; };
-}
