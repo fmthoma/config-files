08d586e79c3c9fa40f4ed9ff5b75172f06971796
Franz Thoma <franz.thoma@tngtech.com>
2023-08-19 01:16:54 +0200

    Extending on configuration

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -3,14 +3,26 @@ import XMonad
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 import XMonad.Layout.Magnifier
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.InsertPosition
+import XMonad.Layout.Accordion
+import XMonad.Layout.Tabbed
 
 main :: IO ()
-main = xmonad myConfig
+main = xmonad $ ewmhFullscreen $ ewmh $ myConfig
 
-myConfig = def { modMask, layoutHook } `additionalKeysP` keymap
+myConfig = def
+    { modMask = mod4Mask
+    , layoutHook
+    , borderWidth = 3
+    , normalBorderColor = "#073642"
+    , focusedBorderColor = "#859900"
+    , manageHook = composeAll
+        [ insertPosition Below Older ]
+    } `additionalKeysP` keymap
   where
     modMask = mod4Mask
-    layoutHook = tiled ||| Mirror tiled ||| Full
+    layoutHook = tiled ||| Mirror tiled ||| simpleTabbed ||| Full
       where
         tiled = magnifiercz' 1.5 $ Tall nmaster delta ratio
         nmaster = 1
