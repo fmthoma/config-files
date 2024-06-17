b334fee93b11e6dbfb1df1e7a9336a80e8cc8cd9
Franz Thoma <franz.thoma@tngtech.com>
2024-06-17 14:12:31 +0200

    wip adaptations

--- nixos/modules/xserver.nix
+++ nixos/modules/xserver.nix
@@ -1,27 +1,33 @@
 { config, pkgs, options, ... }: {
-  config.services.xserver = {
-    enable = true;
-    layout = "de";
-    xkbVariant = "neo";
-    libinput.enable = true;
-    windowManager = {
-      i3 = {
-        enable = true;
-        package = pkgs.i3-gaps;
+  config.services = {
+    xserver = {
+      enable = true;
+      xkb = {
+        layout = "de";
+        variant = "neo";
+      };
+      windowManager = {
+        i3 = {
+          enable = true;
+          package = pkgs.i3-gaps;
+        };
+      };
+      displayManager = {
+        lightdm.enable = true;
+        sessionCommands = ''
+          if test -e $HOME/.Xresources; then
+            ${pkgs.xorg.xrdb}/bin/xrdb --merge $HOME/.Xresources
+          fi
+        '';
       };
     };
     displayManager = {
-      lightdm.enable = true;
       defaultSession = "none+i3";
       autoLogin = {
         user = "thomaf";
         enable = true;
       };
-      sessionCommands = ''
-        if test -e $HOME/.Xresources; then
-          ${pkgs.xorg.xrdb}/bin/xrdb --merge $HOME/.Xresources
-        fi
-      '';
     };
+    libinput.enable = true;
   };
 }
