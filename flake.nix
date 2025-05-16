0f929c0a8f7b7ee42d7af2f206418e2088366a03
Franz Thoma <franz.thoma@tngtech.com>
2025-05-16 23:12:58 +0200

    nixos: Remove old releases

--- flake.nix
+++ flake.nix
@@ -6,9 +6,6 @@
     nixpkgs-23_11.url = "github:nixos/nixpkgs/release-23.11";
     nixpkgs-23_05.url = "github:nixos/nixpkgs/release-23.05";
     nixpkgs-22_11.url = "github:nixos/nixpkgs/release-22.11";
-    nixpkgs-22_05.url = "github:nixos/nixpkgs/release-22.05";
-    nixpkgs-21_11.url = "github:nixos/nixpkgs/release-21.11";
-    nixpkgs-21_05.url = "github:nixos/nixpkgs/release-21.05";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixos";
     keymap-visualizer.url = "github:fmthoma/keymap-visualizer/main";
@@ -19,9 +16,6 @@
     overlays = [
       (_: _: {
         unstable = import inputs.nixpkgs-unstable { system = "x86_64-linux"; inherit overlays; };
-        release-24_05 = import inputs.nixpkgs-24_05 { system = "x86_64-linux"; inherit overlays; };
-        release-23_11 = import inputs.nixpkgs-23_11 { system = "x86_64-linux"; inherit overlays; };
-        release-23_05 = import inputs.nixpkgs-23_05 { system = "x86_64-linux"; inherit overlays; };
         release-22_11 = import inputs.nixpkgs-22_11 { system = "x86_64-linux"; inherit overlays; };
         release-22_05 = import inputs.nixpkgs-22_05 { system = "x86_64-linux"; inherit overlays; };
         release-21_11 = import inputs.nixpkgs-21_11 { system = "x86_64-linux"; inherit overlays; };
